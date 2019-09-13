42143ecf717a8f6fca05609516a9ebc7c7ae68a5
Kathryn Fieg <kathrynfieg@gmail.com>
2019-09-13 23:40:22 +0930

    SRV_Student - student details filled: ID, Name, Email, Qual

--- TafeBuddy_SRV_desktop_App/TafeBuddy_SRV_desktop_App/View/SRV_Student.xaml.cs
+++ TafeBuddy_SRV_desktop_App/TafeBuddy_SRV_desktop_App/View/SRV_Student.xaml.cs
@@ -23,11 +23,29 @@ using Windows.UI.Xaml.Navigation;
 
 namespace TafeBuddy_SRV_desktop_App.View
 {
+    // Class created to handle the object in the ComboBox
+    class Item
+    {
+        public string Id;
+        public string Value;
+        public Item(string id, string value)
+        {
+            Id = id;
+            Value = value;
+        }
+        public override string ToString()
+        {
+            return Value; // The ComboBox actually uses the ToString method to display the text in the control
+        }
+    }
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class SRV_Student : Page
     {
+        private string User = "";
+        private static string StudentID;
 
         public SRV_Student()
         {
@@ -39,6 +57,89 @@ namespace TafeBuddy_SRV_desktop_App.View
             formattableTitleBar.ButtonForegroundColor = Colors.Black;
             CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
             coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            welcomeTxtBlock.Text = "Welcome, " + App.userLogged_firstName + " " + App.userLogged_lastName;
+
+            User = App.userLogged_id;
+
+            PopulateUser();
+            PopulateQualification();
+        }
+
+        /**
+         * Method to populate student details: Name, Qualification, Email
+         **/
+        public void PopulateUser()
+        {
+            // Creates the connection
+            MySqlConnection conn = new MySqlConnection(App.connectionString);
+            // Creates the SQL command
+            MySqlCommand command = new MySqlCommand("SELECT * FROM student WHERE EmailAddress = '" + User + "' OR StudentID = '" + User + "'", conn);
+
+            MySqlDataReader dr; // Creates a reader to read the data
+
+            conn.Open(); // Open the connection
+
+            dr = command.ExecuteReader(); // Execute the command and attach to the reader
+
+            // While there are rows in the read
+            while (dr.Read())
+            {
+                StudentID = dr.GetString("StudentID"); // Stores the StudentID to use in the Competences page
+                studentIdTxtbox.Text = dr.GetString("StudentID");
+                studentEmailTxtbox.Text = dr.GetString("EmailAddress");
+                studentNameTxtbox.Text = dr.GetString("GivenName") + " " + dr.GetString("LastName");
+            }
+
+            // Close the connection
+            conn.Close();
+        }
+
+        /**
+         * Method to populate student qualification combobox
+         **/
+        public void PopulateQualification(string area = "")
+        {
+            //SELECT distinct q.NationalQualCode, q.QualName FROM Student s
+            //INNER JOIN student_studyplan ss ON s.StudentID = ss.StudentID
+            //INNER JOIN Qualification q on ss.QualCode = q.QualCode
+            //WHERE s.EmailAddress = 'kathryn.fieg@student.tafesa.edu.au' OR s.StudentID = '001061267';
+
+            // Creates the connection
+            MySqlConnection conn = new MySqlConnection(App.connectionString);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("SELECT distinct q.NationalQualCode, q.QualName FROM Student s ");
+            sb.Append("INNER JOIN student_studyplan ss ON s.StudentID = ss.StudentID ");
+            sb.Append("INNER JOIN Qualification q on ss.QualCode = q.QualCode ");
+            sb.Append("WHERE (s.EmailAddress = '").Append(User).Append("' ");
+            sb.Append(" OR s.StudentID = '").Append(User).Append("') ");
+
+            //if (!String.IsNullOrEmpty(area))
+            //{
+            //    sb.Append(" AND a.AreasOfStudyID = '").Append(area).Append("' ");
+            //}
+
+            // Creates the SQL command
+            MySqlCommand command = new MySqlCommand(sb.ToString(), conn);
+
+            MySqlDataReader dr; // Creates a reader to read the data
+
+            conn.Open(); // Open the connection
+
+            dr = command.ExecuteReader(); // Execute the command and attach to the reader
+
+            qualificationCmbbox.Items.Clear(); // Clear all the items
+            // While there are rows in the read
+            while (dr.Read())
+            {
+                // Add an item in the Combobox
+                qualificationCmbbox.Items.Add(new Item(dr.GetString("NationalQualCode"), "(" + dr.GetString("NationalQualCode") + ") " + dr.GetString("QualName")));
+                qualificationCmbbox.SelectedItem = qualificationCmbbox.Items[0];
+            }
+
+            // Close the connection
+            conn.Close();
         }
 
         /**
