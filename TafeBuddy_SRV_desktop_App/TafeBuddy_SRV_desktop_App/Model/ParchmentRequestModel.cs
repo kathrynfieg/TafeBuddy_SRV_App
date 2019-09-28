6f7bcb1c8348d6958866070761bfd459b1f2ddb6
Kathryn Fieg <kathrynfieg@gmail.com>
2019-09-28 13:08:28 +0930

    SRV_Admin - "View all parchment requests" feature added

---
+++ TafeBuddy_SRV_desktop_App/TafeBuddy_SRV_desktop_App/Model/ParchmentRequestModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TafeBuddy_SRV_desktop_App.Model
+{
+    public class ParchmentRequestModel
+    {
+        public string StudentID;
+        public string GivenName;
+        public string LastName;
+        public string RequestedQual;
+        public string DateApplied;
+
+        public ParchmentRequestModel(string studentId, string givenName, string lastName, string reqQual, string dateApplied)
+        {
+            this.StudentID = studentId;
+            this.GivenName = givenName;
+            this.LastName = lastName;
+            this.RequestedQual = reqQual;
+            this.DateApplied = dateApplied;
+        }
+    }
+}
